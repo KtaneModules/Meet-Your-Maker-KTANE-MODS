cefa7292c7d607a7c69b3d6afd143ec0ee8aea21
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-06-29 10:45:32 -0400

    Start loading preloaded modules

---
+++ Assets/MasterTemplate/KTANE Scripts/JsonReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader {
+
+	public static bool Success = false;
+	// Use this for initialization
+}
---
+++ Assets/MasterTemplate/KTANE Scripts/MakerModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MakerModule : MonoBehaviour {
+
+	public string ModuleName { get; private set; }
+    public string[] Creators { get; private set; }
+	public Sprite Icon { get; private set; }
+    public static Sprite[] PreloadedIcons;
+    public static bool UsePreloadedIcons;
+
+    public MakerModule(string name, string[] creators)
+    {
+        this.ModuleName = name;
+        this.Creators = creators;
+        if (UsePreloadedIcons)
+        {
+            Icon = PreloadedIcons.First(sprite => sprite.name == name);
+        }
+    }
+}
