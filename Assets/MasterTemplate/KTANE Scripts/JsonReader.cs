7fb52971a7e2bdb8a573f9ea63c69512083c76ff
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-06-29 15:38:22 -0400

    Start loading modules from repo

--- Assets/MasterTemplate/KTANE Scripts/JsonReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class JsonReader {
-
-	public static bool Success = false;
-	// Use this for initialization
-}
